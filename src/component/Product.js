f6971c27ae3a5db66aad416236e1ee6c947d5f8b
Angelamae28 <161902976+JaramilloChristian28@users.noreply.github.com>
2024-04-19 14:17:02 +0800

    Update Product.js

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+
 import '../App.css'; // Import the CSS file
 
 const Product = ({ id, name, description, price, addToCart }) => {
